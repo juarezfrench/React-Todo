199b8cda62df0ce412385293606070f0dcfcd5c3
juarezfrench <markedfrench@gmail.com>
2019-09-30 15:09:23 -0500

    Built TodoList TodoForm and Todo are next.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,37 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class ListForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      item: ''
+    };
+  }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitItem = e => {
+    e.preventDefault();
+    this.props.addItem(this.state.item);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.submitItem}>
+        <input
+          type="text"
+          value={this.item}
+          name="item"
+          onChange={this.handleChanges}
+        />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+export default ListForm;
